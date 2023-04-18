ead8d67107eb77c9f51310739ab880d6459232bc
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-04-18 23:30:19 +0800

    move google fonts to _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+// Google fonts
+import { Inter } from "next/font/google";
+
+// Applies the google font to this homepage
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   // this Components represents the single page
-  return <Component {...pageProps} />;
+  return (
+    // loads the google font
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
