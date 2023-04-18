8ec290fe18fac658cd851b85127fb273f7a8b1a9
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-04-19 00:54:36 +0800

    fetch data from the api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,28 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 
-export default function BreakingNewsPage() {
+import { NewsArticle, NewsResponse } from "@/models/NewsArticle";
+
+interface BreakingNewsPageProps {
+  newsArticles: NewsArticle[];
+}
+// this code will not run on the client only on the server
+export const getServerSideProps: GetServerSideProps<
+  BreakingNewsPageProps
+> = async () => {
+  const response = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=ph&apiKey=" +
+    process.env.NEWS_API_KEY
+  );
+  const newsResponse: NewsResponse = await response.json();
+  return {
+    props: { newsArticles: newsResponse.articles },
+  };
+};
+
+export default function BreakingNewsPage({
+  newsArticles,
+}: BreakingNewsPageProps) {
   return (
     <>
       {/* Add meta data with title changed to */}
@@ -9,6 +31,8 @@ export default function BreakingNewsPage() {
       </Head>
       <main>
         <h1>Breaking News</h1>
+        {/* display the json data */}
+        {JSON.stringify(newsArticles)}
       </main>
     </>
   );
