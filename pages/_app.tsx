7a520e31831c1e437b056f3efd3f64ec767b369d
rjaybernardo <raphaeljaybernardo@gmail.com>
2023-04-21 23:34:13 +0800

    navbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,31 +8,33 @@ import { Container } from "react-bootstrap";
 
 import "@/styles/globals.css";
 import styles from "@/styles/App.module.css";
+import NavBar from "@/components/NavBar";
 
 // Applies the google font to this homepage
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    // loads the google font
-    <div className={inter.className}>
-      {/* meta data */}
-      <Head>
-        <title key="title">NextJs News App</title>
-        <meta
-          name="description"
-          // key is added
-          key="description"
-          content="New App NextJs with Typescript "
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* public/ favicon folder */}
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Container className={styles.pageContainer}>
-        {/* this Components represents the single page */}
-        <Component {...pageProps} />
-      </Container>
-    </div>
-  );
+        return (
+                // loads the google font
+                <div className={inter.className}>
+                        {/* meta data */}
+                        <Head>
+                                <title key="title">NextJs News App</title>
+                                <meta
+                                        name="description"
+                                        // key is added
+                                        key="description"
+                                        content="New App NextJs with Typescript "
+                                />
+                                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                                {/* public/ favicon folder */}
+                                <link rel="icon" href="/favicon.ico" />
+                        </Head>
+                        <NavBar />
+                        <Container className={styles.pageContainer}>
+                                {/* this Components represents the single page */}
+                                <Component {...pageProps} />
+                        </Container>
+                </div>
+        );
 }
